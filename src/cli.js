1067e2143b749f6d0a5ba21674243e732f217f52
BatistaTony <batistatomeoliveira96@gmail.com>
2022-01-03 22:41:29 +0100

    refactor: format doc

--- src/cli.js
+++ src/cli.js
@@ -1,24 +1,28 @@
-import arg from 'arg'
+import arg from "arg";
 
-
-function parseArgsIntoOptions(rawArgs){
-    const args = arg(
-        {
-            '--create': Boolean,
-        "--config": Boolean,
-    "-c": "--create"
-}, {
-            argv: rawArgs.slice(2)
-        }
-    )
-
-    return {
-        create: args['--create'] || false,
-        config: args['--config'] || false
+function parseArgsIntoOptions(rawArgs) {
+  const args = arg(
+    {
+      "--create": Boolean,
+      "--config": Boolean,
+      "-c": "--create",
+      "--template": "Typescript" || "Javascript",
+    },
+    {
+      argv: rawArgs.slice(2),
     }
+  );
+
+  return {
+    create: args["--create"] || false,
+    config: args["--config"] || false,
+    template: args["--template"] || "Javascript",
+  };
 }
 
-export function cli(args){
-    let options  = parseArgsIntoOptions(args)
-    console.log(options)
+
+
+export function cli(args) {
+  let options = parseArgsIntoOptions(args);
+  console.log(options);
 }
