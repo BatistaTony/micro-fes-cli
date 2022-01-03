5fc22b1b50f644aa28b1834bee7c9990284eac06
BatistaTony <batistatomeoliveira96@gmail.com>
2022-01-03 22:36:22 +0100

    feat: create args 4 cli

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,24 @@
+import arg from 'arg'
+
+
+function parseArgsIntoOptions(rawArgs){
+    const args = arg(
+        {
+            '--create': Boolean,
+        "--config": Boolean,
+    "-c": "--create"
+}, {
+            argv: rawArgs.slice(2)
+        }
+    )
+
+    return {
+        create: args['--create'] || false,
+        config: args['--config'] || false
+    }
+}
+
 export function cli(args){
-    console.log(args)
+    let options  = parseArgsIntoOptions(args)
+    console.log(options)
 }
